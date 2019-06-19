2a3ada2cd88d9796934b2c8e80e244881e1a0fe7
sandeepxooa <37658717+sandeepxooa@users.noreply.github.com>
2019-06-19 12:55:06 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -7,5 +7,10 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.24" // ex:  "0.4.20". (Default: Truffle's installed solc)
+    }
+ }
 };
