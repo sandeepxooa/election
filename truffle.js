b1801ce3e9e8e115f8197c8d5c8b20f01e04fe30
Sandeep Patel <37658717+sandeepxooa@users.noreply.github.com>
2019-08-30 10:34:31 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.5.0" // ex:  "0.4.20". (Default: Truffle's installed solc)
+      version: "0.4.0" // ex:  "0.4.20". (Default: Truffle's installed solc)
     }
  }
 };
